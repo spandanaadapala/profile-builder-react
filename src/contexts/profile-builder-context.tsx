7d790ed67af22ba04fb5f046336224580e5a88b6
spandanaadapala <adapala.spandana1@gmail.com>
2024-08-19 15:03:32 +1000

    Sign In design

--- src/contexts/profile-builder-context.tsx
+++ src/contexts/profile-builder-context.tsx
@@ -1,5 +1,9 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 const ProfileBuilderContext = createContext("");
 
+export const profileBuilderContext = () => {
+  const context = useContext(ProfileBuilderContext);
+  return context ?? "required profile builder context";
+};
 export default ProfileBuilderContext;
